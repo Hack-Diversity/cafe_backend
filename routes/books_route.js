452b5c0d54826912acb1ad37c6364873c61845bc
patybn3 <paty.nigro@gmail.com>
2021-02-04 16:41:14 -0500

    Able to get ONE book by ID

--- routes/books_route.js
+++ routes/books_route.js
@@ -9,6 +9,7 @@ const error404 = errors.handle404;
 
 const router = express.Router();
 
+//INDEX get all books
 router.get('/books', (req, res, next) => {
   Book.find()
     .then(books => {
@@ -17,7 +18,13 @@ router.get('/books', (req, res, next) => {
     .then(books => res.status(200).json({ books: books }))
     .catch(next)
 });
-
+//SHOW GET a book
+router.get('/books/:id', (req, res, next) => {
+  Book.findById(req.params.id)
+  .then(error404)
+  .then(book => res.status(200).json({ book: book.toObject()}))
+  .catch(next)
+})
 
 // CREATE
 // POST /surveys
@@ -25,7 +32,7 @@ app.post('/create-books', (req, res, next) => {
   // set owner of new survey to be current user
   const book = req.body
 
-  Survey.create(book)
+  Book.create(book)
     // respond to succesful `create` with status 201 and JSON of new "survey"
     .then(book => {
       res.status(201).json({ book: book.toObject() })
