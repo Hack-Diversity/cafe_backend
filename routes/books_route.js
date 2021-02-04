cdca22d9481370ddb4a894373a108cb48fe56007
patybn3 <paty.nigro@gmail.com>
2021-02-04 05:25:47 -0500

    Final changes of the day, review previous commits

--- routes/books_route.js
+++ routes/books_route.js
@@ -17,4 +17,23 @@ router.get('/books', (req, res, next) => {
     .catch(next)
 });
 
+
+// CREATE
+// POST /surveys
+router.post('/books', (req, res, next) => {
+  // set owner of new survey to be current user
+  const book = req.body.book
+
+  Survey.create(book)
+    // respond to succesful `create` with status 201 and JSON of new "survey"
+    .then(book => {
+      res.status(201).json({ book: book.toObject() })
+    })
+    // if an error occurs, pass it off to our error handler
+    // the error handler needs the error message and the `res` object so that it
+    // can send an error message back to the client
+    .catch(next)
+})
+
+
 module.exports = router;
